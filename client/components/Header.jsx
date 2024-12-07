acc45982173b526c89e4e84e1081d7591c8ed1d8
Himashaa <himashanethmini05@gmail.com>
2024-12-07 23:26:07 +0530

    complete the header

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import css from '../styles/Header.module.css'
+import Logo from "../assets/Logo.png";
+import { UilShoppingBag } from '@iconscout/react-unicons'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className={css.header}>
+
+      {/* logo side */}
+      <div className={css.logo}>
+        <Image src={Logo} alt="" width={50} height={50} />
+        <span>Hima Florist</span>
+      </div>
+
+      {/* menu bar */}
+      <ul className={css.menu}>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Contact</li>
+      </ul>
+
+      {/*  cart side*/}
+      <div className={css.cartSide}>
+        <div className={css.cart}>
+          <UilShoppingBag />
+        </div>
+      </div>
+    </div>
   )
 }
 
