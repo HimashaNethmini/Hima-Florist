5852de2033f334673d1e2e120e18e05a651affc8
Himashaa <himashanethmini05@gmail.com>
2024-12-09 23:18:20 +0530

    added footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { UilFacebook, UilGithub, UilX } from '@iconscout/react-unicons'
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={CSS.container}>
+      <span>ALL RIGHTS RESERVED</span>
+      <div className={css.social}>
+        <UilFacebook size={35} />
+        <UilX size={35} />
+        <UilGithub size={35} />
+      </div>
+
+      <div className={css.logo}>
+        <Image src={Logo} alt="" width={50} height={50} />
+        <span>Hima Florist</span>
+      </div>
+    </div>
   )
 }
 
