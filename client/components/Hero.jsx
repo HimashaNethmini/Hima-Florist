bddd43c8a0ece55088fed0cb0eb282a0f057e1bb
Himashaa <himashanethmini05@gmail.com>
2024-12-11 23:12:40 +0530

    make the hero section

--- client/components/Hero.jsx
+++ client/components/Hero.jsx
@@ -1,16 +1,30 @@
-import css from '../styles/Hero.module.css'
+import css from "../styles/Hero.module.css";
+import Image from "next/image";
+import xmas from "../assets/xmas.png";
+import jingle from "../assets/jingle.png";
 
 const Hero = () => {
   return (
     <div className={css.container}>
-
-        <div className={css.left}>
-            <span>Prettier than ever</span>
+      <div className={css.left}>
+        <div className={css.flower}>
+          <span>Prettier than ever</span>
+          <Image src={xmas} alt="" width={40} height={40} />
         </div>
 
-        <div className={css.right}></div>
-    </div>
-  )
-}
+        <div className={css.containerText}>
+          <span> To Your</span>
+          <span style={{ color: "var(--themeRed)" }}>Doorsteps </span>
+          <Image src={jingle} alt="" height={40} width={40} />
+        </div>
 
-export default Hero
+        <button className="btn" >Get Started</button>
+      </div>
+
+      {/* right side */}
+      <div className={css.right}></div>
+    </div>
+  );
+};
+
+export default Hero;
