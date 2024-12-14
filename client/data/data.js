24638efe14c2f9e4950161734b35648c55b4c1e0
Himashaa <himashanethmini05@gmail.com>
2024-12-14 22:34:49 +0530

    add mock data

---
+++ client/data/data.js
@@ -0,0 +1,29 @@
+const flowers = [
+{
+    id: 1,
+    pic: "../assets/image(5).jpg",
+    name: "Rosemary Bouquet",
+    price :"$5.99"
+},
+
+{
+    id: 2,
+    pic: "../assets/image(2).jpg",
+    name: "Rosemary Bouquet",
+    price :"$5.99"
+},
+
+{
+    id: 3,
+    pic: "../assets/image(3).jpg",
+    name: "Rosemary Bouquet",
+    price :"$5.99"
+},
+
+{
+    id: 4,
+    pic: "../assets/image(4).jpg",
+    name: "Rosemary Bouquet",
+    price :"$5.99"
+},
+]
