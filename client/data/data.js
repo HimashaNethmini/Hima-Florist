8940d213dfb209a0ecdf19f1d523f913e0560604
Himashaa <himashanethmini05@gmail.com>
2024-12-15 12:58:40 +0530

    added images urls

--- client/data/data.js
+++ client/data/data.js
@@ -1,29 +1,35 @@
-const flowers = [
-{
-    id: 1,
-    pic: "../assets/image(5).jpg",
-    name: "Rosemary Bouquet",
-    price :"$5.99"
-},
+import Image from "next/image";
 
-{
-    id: 2,
-    pic: "../assets/image(2).jpg",
-    name: "Rosemary Bouquet",
-    price :"$5.99"
-},
+const data = {
+  flowers: [
+    {
+      id: 1,
+      pic: "https://i.pinimg.com/736x/48/c8/11/48c8116fbd3bea69ab37a5998c01516c.jpg",
+      name: "Rosemary Bouquet",
+      price: "5.99",
+    },
 
-{
-    id: 3,
-    pic: "../assets/image(3).jpg",
-    name: "Rosemary Bouquet",
-    price :"$5.99"
-},
+    {
+      id: 2,
+      pic: "https://i.pinimg.com/736x/ae/5c/31/ae5c31f9b4ffd04e0bf3fdab62cb095b.jpg",
+      name: "Rosemary Bouquet",
+      price: "5.99",
+    },
 
-{
-    id: 4,
-    pic: "../assets/image(4).jpg",
-    name: "Rosemary Bouquet",
-    price :"$5.99"
-},
-]
+    {
+      id: 3,
+      pic: "https://i.pinimg.com/736x/eb/df/0e/ebdf0ef456d288c55aa72a9574f07090.jpg",
+      name: "Rosemary Bouquet",
+      price: "5.99",
+    },
+
+    {
+      id: 4,
+      pic: "https://i.pinimg.com/736x/b2/c4/c0/b2c4c0e4f26ddab81c1ff00646e339c1.jpg",
+      name: "Rosemary Bouquet",
+      price: "5.99",
+    },
+  ],
+};
+
+export default data;
