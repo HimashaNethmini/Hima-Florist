93d6ab6a719c059bd3b4b6eaf0ef03c33e02cffa
Himashaa <himashanethmini05@gmail.com>
2024-12-15 14:01:11 +0530

    completed

--- client/components/Collection.jsx
+++ client/components/Collection.jsx
@@ -1,26 +1,44 @@
-import React from "react";
 import css from "../styles/Collection.module.css";
+import data from "../data/data";
+import Image from "next/image";
 
 const Collection = () => {
+  const { flowers } = data; // Destructure flowers from data
+  console.log(flowers);
+
   return (
     <>
       <div className={css.container}>
         <div className={css.heading}>
-          <h2>OUR COLLECTION</h2>
+          <h1>OUR COLLECTION</h1>
           <span>
-            True friendship is like a
-            rose: we don't realize its beauty until it fades
+            True friendship is like a rose: we don't realize its beauty until it
+            fades
           </span>
-          <span>Contact us</span>
+          {/* <span>Contact us</span> */}
         </div>
 
-        {/* collection */}
-        <div className={css.flowers}>
-            <div className={imageContainer}>
-                {/* <Image src = {} alt="" /> */}
-
-
+        {/* collection -mapping */}
+        <div className={css.collection}>
+          {flowers.map((flower, index) => (
+            <div className={css.flowers} key={flower.id}>
+              <div className={css.imageContainer}>
+                <Image
+                  src={flower.pic}
+                  alt="flower_bouquets"
+                  width={225}
+                  height={250}
+                  objectFit="cover"
+                  style={{ borderRadius: "2rem" }}
+                />
+              </div>
+              <span>{flower.name}</span>
+              <span> 
+                <span style = {{color: "var(--themeRed)" }}> $ </span>
+                {flower.price}
+              </span>
             </div>
+          ))}
         </div>
       </div>
     </>
