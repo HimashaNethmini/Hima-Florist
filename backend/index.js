ba9a2630242338fd8f1ee496ef61dfcf5fc88c0f
Himashaa <himashanethmini05@gmail.com>
2025-01-06 23:20:58 +0530

    Added api integration

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
+import express from "express"
 import dotenv from "dotenv";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 dotenv.config()
+
+const app = express();
+const port = process.env.PORT;
 
 const gemini_api_key = process.env.API_KEY;
 const googleAI = new GoogleGenerativeAI(gemini_api_key);
@@ -16,16 +20,20 @@ const geminiModel = googleAI.getGenerativeModel({
   model: "gemini-pro",
   geminiConfig,
 });
- 
+
 const generate = async () => {
-  try {
-    const prompt = "Tell me about Himasha Nethmini.";
-    const result = await geminiModel.generateContent(prompt);
+    try {
+    const prompt = "Tell me about Roses.";
+      const result = await geminiModel.generateContent(prompt);
     const response = result.response;
-    console.log(response.text());
-  } catch (error) {
-    console.log("response error", error);
-  }
+      console.log(response.text());
+    } catch (error) {
+      console.log("response error", error);
+    }
 };
  
 generate();
+
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+})
